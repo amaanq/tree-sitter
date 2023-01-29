faae7617506b91fff71b4502b3b4f3c5f7504d62
Andrew Hlynskyi <ahlincq@gmail.com>
2023-01-29 18:42:37 +0200

    cli/query: implement colored rendering [1/*]

--- cli/src/query.rs
+++ cli/src/query.rs
@@ -1,4 +1,4 @@
-use crate::query_testing;
+use crate::{query_testing, render};
 use anyhow::{Context, Result};
 use std::{
     fs,
@@ -23,6 +23,12 @@ pub fn query_files_at_paths(
         .with_context(|| format!("Error reading query file {:?}", query_path))?;
     let query = Query::new(language, &query_source).with_context(|| "Query compilation failed")?;
 
+    let max_capture_len =
+        query
+            .capture_names()
+            .iter()
+            .fold(0usize, |acc, e| if e.len() > acc { e.len() } else { acc });
+
     let mut query_cursor = QueryCursor::new();
     if let Some(range) = range {
         query_cursor.set_byte_range(range);
@@ -30,6 +36,8 @@ pub fn query_files_at_paths(
 
     let mut parser = Parser::new();
     parser.set_language(language)?;
+
+    let c = render::Colors::new();
 
     for path in paths {
         let mut results = Vec::new();
@@ -39,6 +47,8 @@ pub fn query_files_at_paths(
         let source_code =
             fs::read(&path).with_context(|| format!("Error reading source file {:?}", path))?;
         let tree = parser.parse(&source_code, None).unwrap();
+
+        let mut last_row = usize::MAX;
 
         if ordered_captures {
             for (m, capture_index) in
@@ -56,11 +66,20 @@ pub fn query_files_at_paths(
                     row: end_row,
                     column: end_column,
                 } = capture.node.end_position();
-                let pos = format!("{start_row:3}:{start_column:<2} - {end_row}:{end_column:<3}");
+                let pos_c = if start_row != last_row {
+                    last_row = start_row;
+                    c.pos1
+                } else {
+                    c.pos2
+                };
+                let pos = format!("{start_row}:{start_column:<3} - {end_row}:{end_column}");
                 let capture_text = capture.node.utf8_text(&source_code).unwrap_or("");
                 writeln!(
                     &mut stdout,
-                    "{pos:<15} pattern: {pattern_index:>2}, capture: {capture_index:<2} - {capture_name}, text: `{capture_text}`"
+                    // "{pos:<15} {pattern_index:>2}:{capture_index:<2} {capture_name:>20} `{capture_text}`"
+                    "{P}{pos:<18} {PI}{pattern_index:>3}{CL}:{CI}{capture_index:<3} {CN}{capture_name:<max_capture_len$} {BK}`{CT}{capture_text}{BK}`{R}",
+                    P=pos_c.prefix(), PI=c.field.prefix(), CL=c.text.prefix(), CI=c.nonterm.prefix(),
+                    CN=c.bytes.prefix(), CT=c.text.prefix(), BK=c.backtick.prefix(), R=c.backtick.suffix(),
                 )?;
                 results.push(query_testing::CaptureInfo {
                     name: capture_name.to_string(),
@@ -83,15 +102,25 @@ pub fn query_files_at_paths(
                         row: end_row,
                         column: end_column,
                     } = capture.node.end_position();
-                    let pos = format!("{start_row:3}:{start_column} - {end_row}:{end_column:<3}");
+                    let pos_c = if start_row != last_row {
+                        last_row = start_row;
+                        c.pos1
+                    } else {
+                        c.pos2
+                    };
+                    let pos = format!("{start_row}:{start_column:<3} - {end_row}:{end_column}");
                     if end_row == start_row {
                         let capture_text = capture.node.utf8_text(&source_code).unwrap_or("");
                         writeln!(
                             &mut stdout,
-                            // "  {pos:<15} capture: {capture_index:2} - {capture_name}, text: `{capture_text}`")?;
-                            "{pos:<15} pattern: {pattern_index:>2}, capture: {capture_index:2} - {capture_name}, text: `{capture_text}`")?;
+                            // "  {pos:<15} capture: {capture_index:2} - {capture_name}, text: `{capture_text}`"
+                            // "{pos:<15} {pattern_index:>2}:{capture_index:<2} {capture_name:>20} `{capture_text}`"
+                            "{P}{pos:<18} {PI}{pattern_index:>3}{CL}:{CI}{capture_index:<3} {CN}{capture_name:<max_capture_len$} {BK}`{CT}{capture_text}{BK}`{R}",
+                            P=pos_c.prefix(), PI=c.field.prefix(), CL=c.text.prefix(), CI=c.nonterm.prefix(),
+                            CN=c.bytes.prefix(), CT=c.text.prefix(), BK=c.backtick.prefix(), R=c.backtick.suffix(),
+                        )?;
                     } else {
-                        writeln!(&mut stdout, "    {pos:<15} capture: {capture_name}",)?;
+                        writeln!(&mut stdout, "    {pos:<15} {capture_name}")?;
                     }
                     results.push(query_testing::CaptureInfo {
                         name: capture_name.to_string(),
