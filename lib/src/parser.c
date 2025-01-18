9756598229134a8f7e384d8546535c35c7353f82
Amaan Qureshi <amaanq12@gmail.com>
2025-01-17 23:42:47 -0500

    test

--- lib/src/parser.c
+++ lib/src/parser.c
@@ -1848,17 +1848,23 @@ static unsigned ts_parser__condense_stack(TSParser *self) {
 static bool ts_parser__balance_subtree(TSParser *self) {
   Subtree finished_tree = self->finished_tree;
 
-  array_clear(&self->tree_pool.tree_stack);
-
-  if (!self->canceled_balancing && ts_subtree_child_count(finished_tree) > 0 && finished_tree.ptr->ref_count == 1) {
-    array_push(&self->tree_pool.tree_stack, ts_subtree_to_mut_unsafe(finished_tree));
+  // If we haven't canceled balancing in progress before, then we want to clear the tree stack and
+  // push the initial finished tree onto it. Otherwise, if we're resuming balancing after a
+  // cancellation, we don't want to clear the tree stack.
+  if (!self->canceled_balancing) {
+    array_clear(&self->tree_pool.tree_stack);
+    if (ts_subtree_child_count(finished_tree) > 0 && finished_tree.ptr->ref_count == 1) {
+      array_push(&self->tree_pool.tree_stack, ts_subtree_to_mut_unsafe(finished_tree));
+    }
   }
 
   while (self->tree_pool.tree_stack.size > 0) {
     if (!ts_parser__check_progress(self, NULL, NULL)) {
       return false;
     }
-    MutableSubtree tree = array_pop(&self->tree_pool.tree_stack);
+    MutableSubtree tree = self->tree_pool.tree_stack.contents[
+      self->tree_pool.tree_stack.size - 1
+    ];
 
     if (tree.ptr->repeat_depth > 0) {
       Subtree child1 = ts_subtree_children(tree)[0];
@@ -1869,9 +1875,15 @@ static bool ts_parser__balance_subtree(TSParser *self) {
         for (unsigned i = n / 2; i > 0; i /= 2) {
           ts_subtree_compress(tree, i, self->language, &self->tree_pool.tree_stack);
           n -= i;
+          if (!ts_parser__check_progress(self, NULL, NULL)) {
+            return false;
+          }
         }
       }
     }
+
+    array_pop(&self->tree_pool.tree_stack);
+
 
     for (uint32_t i = 0; i < tree.ptr->child_count; i++) {
       Subtree child = ts_subtree_children(tree)[i];
